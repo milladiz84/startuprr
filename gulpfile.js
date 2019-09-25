761e132a8b2a24e4c0079a24f70407921fa8a705
milladiz84 <milladiz84@gmail.com>
2019-09-25 16:37:05 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const browserSync = require('browser-sync').create();
+
+
+
+/*gulp.task('mytask', async function(){
+	//console.log('привет, я таск!');
+	return gulp.src('source-files')
+	.pipe(plugin())
+	.pipe(gulp.dest('folder'))
+});
+*/
+
+
+gulp.task('js', async function() {
+	return gulp.src('js/*.js')
+		.pipe(plumber())
+		.pipe(rigger())
+        .pipe(gulp.dest('app/js/'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(gulp.dest('app/js/'))
+        .pipe(browserSync.stream());
+    });
+
+
+gulp.task('sass', async function() {
+	//return gulp.src('app/sass/**/*.sass')
+	//return gulp.src('app/sass/*.sass')
+	//return gulp.src('!app/sass/main.sass') "!" - исключает этот файл, не будет обрабатыватьсыся//
+	//return gulp.src('[!app/sass/main.sass', 'app/sass/**/*.sass']) все файлы будут обрабатываться, кроме main.sass//
+	//return gulp.src('app/sass/*.+(sccs|sass)') выбираем все scss и sass файлы только в директории sass//
+	//return gulp.src('app/sass/**/*.+(sccs|sass)') выбираем все scss и sass файлы во всех поддиректориях//
+	return gulp.src('app/sass/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css/'))
+	.pipe(browserSync.stream());
+	});
+
+gulp.task('watch', gulp.series('sass', function() {
+ 
+    browserSync.init({
+        server: "app/"
+    });
+ 
+    gulp.watch("app/sass/*.scss", gulp.series('sass'));
+    gulp.watch("bootstrap/scss/**/*.scss", gulp.series('sass'));
+    gulp.watch('app/css/*.css').on('change', browserSync.reload);
+    gulp.watch("js/*.js").on('change', gulp.series('js'));
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+}));
---
+++ js/main.js
@@ -0,0 +1,5 @@
+//= ../jquery/dist/jquery.js
+//= ../popper.js/dist/umd/popper.js
+//= ../bootstrap/js/dist/util.js
+//= ../bootstrap/js/dist/tooltip.js
+//= custom/tooltip.js
